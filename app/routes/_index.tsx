2d51b7cd6e6113642b2dfda475853e87e8028ebd
sena-m09 <sena-murakami@gaji.jp>
2024-10-29 10:23:22 +0900

    add: ログイン時にユーザーid を表示する

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
-import {
-  type MetaFunction,
-} from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { type MetaFunction, type LoaderFunctionArgs } from "@remix-run/node";
+import { Link, Form, json, useLoaderData } from "@remix-run/react";
+import { supabaseClient } from "~/services/supabase.server";
 
 
 export const meta: MetaFunction = () => {
@@ -11,12 +10,24 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async({ request }: LoaderFunctionArgs) => {
+  const { supabase } = supabaseClient(request);
+  const { data: { user } } = await supabase.auth.getUser();
+
+  return json({ user });
+}
+
 export default function Index() {
+  const { user } = useLoaderData<typeof loader>();
 
   return (
     <>
       <h1>トップページ</h1>
+      {user && <p>Welcome, {user.id}</p>}
       <Link to={"/login"}>ログインページへ</Link>
+      <Form action="/auth/signout" method="post">
+        <button type="submit">ログアウト</button>
+      </Form>
     </>
   );
 }
