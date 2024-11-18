7092ef475e3bab4d2a2ec76cf53e0a87e6dd0b2f
sena-m09 <sena-murakami@gaji.jp>
2024-11-18 22:20:59 +0900

    chore: 不要なものを削除

--- app/models/auth.ts
+++ app/models/auth.ts
@@ -1,33 +1,10 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { z } from "zod";
 
-/**
- * ログイン処理のクエリパラメータの型定義
- */
-export type SignInQueryParams = {
-  /**
-   * トークンハッシュ値
-   */
-  tokenHash: string | null;
-  /**
-   * 認証のタイプ
-   */
-  type: EmailOtpType | null;
-  /**
-   * リダイレクト先URL
-   */
-  nextUrl: string;
-};
-
 export const EmailOtpTypeSchema = z.enum(["signup", "invite", "magiclink", "recovery", "email_change", "email"])satisfies z.ZodType<EmailOtpType>;
 
 export const PathSchema = z.string().regex(/^\/[a-zA-Z0-9-_/]*$/);
 
-/**
- * サインアップ確認時のクエリパラメータのスキーマ
- * 
- * TODO: next_url のバリデーションを追加する
- */
 export const SingUpConfirmQueryParamsSchema = z.object({
   token_hash: z.string(),
   type: EmailOtpTypeSchema,
--- app/routes/auth.confirm/route.tsx
+++ app/routes/auth.confirm/route.tsx
@@ -1,31 +1,9 @@
 import { LoaderFunctionArgs, redirect } from "@remix-run/node";
-import { EmailOtpType } from "@supabase/supabase-js";
 import { supabaseClient } from "~/services/supabase.server";
-import { SignInQueryParams, SingUpConfirmQueryParamsSchema } from "~/models/auth";
+import { SingUpConfirmQueryParamsSchema } from "~/models/auth";
 import { err, ok, ResultAsync } from "neverthrow";
 
-/**
- * ログイン時のクエリパラメータを取得する関数。
- *
- * @param {URL} url - ローダー関数で受け取ったリクエストURL
- * @returns {{tokenHash: string, type: string, nextUrl: string}} クエリパラメータをひとまとめにしたオブジェクト。
- *
- * @example
- * ```typescript
- * const signInQueryParams = getSignInQueryParams(new URL(request.url));
- * ```
- */
-const getQueryParams = (url: URL): SignInQueryParams => {
-  const tokenHash = url.searchParams.get("token_hash");
-  const type = url.searchParams.get("type") as EmailOtpType | null;
-  const nextUrl = url.searchParams.get("next") ?? "/";
-
-  return { tokenHash, type, nextUrl };
-};
-
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  // クエリパラメータから必要な情報を取得
-  const signInQueryParams = getQueryParams(new URL(request.url));
   const url = new URL(request.url);
   const tokenHash = url.searchParams.get("token_hash");
   const type = url.searchParams.get("type");
