30ded9d83be5dad1e024e0d7e60288f55660dc2e
sena-m09 <sena-murakami@gaji.jp>
2024-11-23 17:35:28 +0900

    chore: format 実行

--- app/routes/auth.signout/route.tsx
+++ app/routes/auth.signout/route.tsx
@@ -1,30 +1,28 @@
 import { type ActionFunctionArgs, redirect } from "@remix-run/node";
+import { ResultAsync } from "neverthrow";
 
 import { supabaseClient } from "~/services/supabase.server";
-import { ResultAsync } from "neverthrow";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { supabase, headers } = supabaseClient(request);
 
-  return ResultAsync.fromPromise(
-    supabase.auth.signOut(),
-    (error) => error
-  ).map((response) => {
-    if (response.error) {
-      console.error("Sign out error:", response.error);
-      throw new Error(response.error.message);
-    }
-  })
-  .match(
-    () => {
-      return redirect("/", {
-        headers,
-      });
-    },
-    (error) => {
-      // TODO: エラーハンドリングを追加すること
-      console.log(error);
-      return redirect("/error");
-    }
-  )
+  return ResultAsync.fromPromise(supabase.auth.signOut(), (error) => error)
+    .map((response) => {
+      if (response.error) {
+        console.error("Sign out error:", response.error);
+        throw new Error(response.error.message);
+      }
+    })
+    .match(
+      () => {
+        return redirect("/", {
+          headers,
+        });
+      },
+      (error) => {
+        // TODO: エラーハンドリングを追加すること
+        console.log(error);
+        return redirect("/error");
+      },
+    );
 };
