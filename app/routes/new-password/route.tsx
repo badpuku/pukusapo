a9758dcac8c3a2e38786ce74061daa94aadc7a05
sugawara-kouki <kou.720131@gmail.com>
2025-01-09 22:36:49 +0900

    feat: 新パスワード入力画面を作成

---
+++ app/routes/new-password/route.tsx
@@ -0,0 +1,14 @@
+import { Form } from "@remix-run/react";
+
+export default function NewPasswordRoute() {
+  return (
+    <main className="p-6">
+      <h1 className="mb-1">リセットパスワード入力ページ</h1>
+      <p>リセットするパスワードを入力する</p>
+      <Form action="/auth/reset-password" method="post">
+        <input name="password" type="password" placeholder="パスワード" />
+        <button type="submit">パスワード変更</button>
+      </Form>
+    </main>
+  );
+}
