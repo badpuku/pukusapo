c4351c196b52e98a110ea31c03d31efbc0f86feb
sugawara-kouki <kou.720131@gmail.com>
2025-01-19 18:23:35 +0900

    fix: パスワードリセットリクエストのルートを変更

--- app/routes/reset-password/route.tsx
+++ app/routes/reset-password/route.tsx
@@ -5,7 +5,7 @@ export default function ResetPasswordRoute() {
     <main className="p-6">
       <h1 className="mb-1">パスワードリセットページ</h1>
       <p>パスワードリセットをおこなうアカウントのメールアドレスを入力する</p>
-      <Form action="/auth/reset" method="post">
+      <Form action="/auth/password-reset/send" method="post">
         <input name="email" type="email" placeholder="sample@example.com" />
         <button type="submit">パスワード変更メール送信</button>
       </Form>
