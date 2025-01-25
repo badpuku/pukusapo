a53d3f41d4115ea7fd657e93eb84648763dd66a3
sugawara-kouki <kou.720131@gmail.com>
2025-01-25 21:58:43 +0900

    fix: イベントテーブルのスキーマ作成とそれに伴うusersテーブルのスキーマ修正

--- supabase/migrations/20250111155406_initialize_auth.sql
+++ supabase/migrations/20250111155406_initialize_auth.sql
@@ -2,8 +2,13 @@
 create table public.users (
   id uuid primary key references auth.users on delete cascade,
   email text not NULL,
+  role varchar(50) default 'user',
   first_name text,
-  last_name text
+  last_name text,
+  created_by uuid references public.users(id),
+  created_at timestamp with time zone default now(),
+  updated_by uuid references public.users(id),
+  updated_at timestamp with time zone default now()
 );
 
 -- users テーブルへのRLSの設定
@@ -21,7 +26,6 @@ create policy "Allow update for users themselves."
   on public.users for update using (
     auth.uid() = id
   );
-
 
 -- auth.users をコピーする関数の定義
 create function public.handle_new_user()
@@ -41,3 +45,32 @@ $$;
 create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
+
+-- public スキーマにeventsテーブルを作成する
+-- TODO:: トリガー等は後から追加する
+create table public.events (
+  id uuid primary key default gen_random_uuid(),
+  title varchar(50) not NULL,
+  description text,
+  start_time timestamp with time zone,
+  end_time timestamp with time zone,
+  max_participants integer,
+  created_by uuid references public.users(id),
+  created_at timestamp with time zone default now(),
+  updated_by uuid references public.users(id),
+  updated_at timestamp with time zone default now(),
+  status varchar(20) -- 'draft', 'published', 'closed'
+);
+
+-- public スキーマにイベント登録者情報テーブルを作成する
+create table public.event_registrations (
+  user_id uuid references public.users(id),
+  event_id uuid references public.events(id),
+  registered_at timestamp with time zone default now(),
+  created_at timestamp with time zone default now(),
+  created_by uuid references public.users(id),
+  updated_at timestamp with time zone default now(),
+  updated_by uuid references public.users(id),
+  status integer, -- 'registered', 'cancelled' TODO:: ここだけ数字での管理でいいのか
+  primary key (event_id, user_id)
+);
