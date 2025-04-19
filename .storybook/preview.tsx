363faeecab53f81d36adf72b1c1d2b67ec50542b
sena-m09 <sena-murakami@gaji.jp>
2025-04-20 01:11:28 +0900

    feat: storybook の設定調整

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,5 @@
 import type { Preview } from "@storybook/react";
-import React from "react";
 import "../app/app.css";
-import { createRoutesStub } from "react-router";
-
 
 const preview: Preview = {
   parameters: {
@@ -14,18 +11,6 @@ const preview: Preview = {
     },
     layout: "fullscreen",
   },
-  decorators: [
-    (Story) => {
-      const ReactRouterStub = createRoutesStub([
-        {
-          path: "/*",
-          Component: () => <Story />,
-        },
-      ]);
-
-      return <ReactRouterStub />;
-    },
-  ],
 };
 
 export default preview;
