70a41562a0e1922d8d657309d99642971cbc2fc5
sena-m09 <sena-murakami@gaji.jp>
2025-06-26 00:28:05 +0900

    feat: ルーティング設定を更新

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,6 @@
-import { index, route, type RouteConfig } from "@react-router/dev/routes";
-// import { flatRoutes } from "@react-router/fs-routes";
+import { index, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
   index("./routes/_index.tsx"),
-  route("api/webhooks/clerk/user", "./routes/api/webhooks/clerk/user/route.ts"),
   // ...(await flatRoutes()),
 ] satisfies RouteConfig;
