29d930c2942cddea03c5a390680aa2d2947149e7
sena-m09 <sena-murakami@gaji.jp>
2025-09-28 18:33:33 +0900

    feat: Clerk no導入で不要になったファイルを削除

--- app/routes/password-reset.requested/route.tsx
+++
@@ -1,8 +0,0 @@
-export default function PasswordResetRequestedRoute() {
-  return (
-    <>
-      <h1>パスワードリセット用のメールを送信しました。</h1>
-      <p>メールを確認してください</p>
-    </>
-  );
-}
--- app/routes/welcome/route.tsx
+++
@@ -1,8 +0,0 @@
-export default function WelcomeRoute() {
-  return (
-    <>
-      <h1>認証用のメールを送信しました。</h1>
-      <p>メールを確認してください</p>
-    </>
-  );
-}
