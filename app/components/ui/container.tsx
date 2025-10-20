4592fd1466aae5c1b8abea6ac7de1fcf77307a88
sena-m09 <sena-murakami@gaji.jp>
2025-10-20 23:08:34 +0900

    feat: border を削除

--- app/components/ui/container.tsx
+++ app/components/ui/container.tsx
@@ -1,7 +1,7 @@
 import { cn } from "~/lib/utils";
 
 function Container({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={cn("w-full border-1 border-zinc-200 bg-white p-4", className)} {...props} />;
+  return <div className={cn("w-full bg-white p-6", className)} {...props} />;
 }
 
 export { Container };
