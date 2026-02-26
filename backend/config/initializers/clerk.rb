981b3cc076ef7dfbf2d70fc2469baa80e50d7183
sena-m09 <sena-murakami@gaji.jp>
2026-02-26 09:26:21 +0900

    feat: clerk publish key も必要だったため追加

--- backend/config/initializers/clerk.rb
+++ backend/config/initializers/clerk.rb
@@ -1,5 +1,7 @@
 Clerk.configure do |c|
   c.secret_key = ENV["CLERK_SECRET_KEY"]
+  c.publishable_key = ENV['CLERK_PUBLISHABLE_KEY']
+  
   if Rails.env.development?
     c.logger = Logger.new($stdout)
   end
