cf933462056b7b9c27cc1c9b8f1e67395245a6a4
sena-m09 <sena-murakami@gaji.jp>
2026-03-31 09:27:23 +0900

    feat: モデルのスペック追加

---
+++ backend/spec/models/role_permission_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe RolePermission do
+  describe 'associations' do
+    it { is_expected.to belong_to(:role) }
+    it { is_expected.to belong_to(:permission) }
+  end
+
+  describe 'composite primary key' do
+    it 'uses [role_id, permission_id] as primary key' do
+      expect(described_class.primary_key).to eq(["role_id", "permission_id"])
+    end
+  end
+end
--- backend/spec/models/role_spec.rb
+++ backend/spec/models/role_spec.rb
@@ -1,5 +1,9 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Role do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:profiles) }
+    it { is_expected.to have_many(:role_permissions).dependent(:destroy) }
+    it { is_expected.to have_many(:permissions).through(:role_permissions) }
+  end
 end
